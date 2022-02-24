b5df211342d8211131d5f8c6e6c08653698dce66
Raghupriyanth <raghupriyanth@gmail.com>
2022-02-24 12:48:51 +0530

    User login stuff files

---
+++ WebContent/login.js
@@ -0,0 +1,172 @@
+$(document).ready(function () {
+  let email, sessionInfo;
+  $("#next").click(function () {
+    email = $("#userName").val();
+    if ($("#userName").val() == null || $("#userName").val() == "") {
+      $("#result1").html("Please Enter your Username").css("color", "red");
+      $("#userName").css("border", "2px solid #ef476f");
+    } else {
+      $.ajax({
+        type: "POST",
+        data: { useremail: email },
+        url: "emailvalidate",
+        async: false,
+        success: function (result) {
+          if ($.trim(result) == "true") {
+            $("#username").toggleClass("hidden");
+            $("#password").toggleClass("show");
+            $("#pass_select").addClass("animate_sel");
+            $("#pass1").addClass("animate_pass");
+          }
+          if ($.trim(result) == "false") {
+            $("#result1")
+              .html(
+                "This Email ID cannot be found. Please use a different Email ID or <span style='padding-left: 5px;'><a href='register'> sign up</a></span> for a new account."
+              )
+              .css("color", "red");
+            $("#userName").css("border", "2px solid red");
+            let email = document.getElementById("userName").value;
+            sessionStorage.setItem("email", email);
+          }
+        },
+      });
+    }
+  });
+
+  $("#userName").focus(function () {
+    $("#result1").html("");
+    $("#userName").css("border", "");
+  });
+
+  const passwordResult = message => {
+    $("#user_container").removeClass("hidden");
+    $("#user_container").addClass("show");
+    $("#loader_container").removeClass("show");
+    $("#loader_container").addClass("hidden");
+    $("#pass-result").html(message).css("color", "red");
+    $("#pass1").css("border", "2px solid #d90429");
+  };
+
+  $("#btnsubmit").click(function () {
+    const password = $("#pass1").val();
+    if ($.trim(password) == null || $.trim(password) == "") {
+      passwordResult("Please Enter your password");
+    } else {
+      $("#user_container").addClass("hidden");
+      $("#loader_container").removeClass("hidden");
+      $("#loader_container").addClass("show");
+      console.log("pasword else");
+      $.ajax({
+        type: "POST",
+        data: { Password: password, useremail: email },
+        url: "passwordvalidate",
+        async: false,
+        success: function (value) {
+          console.log(value);
+          if (value.ServerError === true) {
+            passwordResult(
+              "Problem in our end We will rectify it soon. Kindly try after sometime"
+            );
+          } else if (value.mfapassed === false) {
+            passwordResult("Incorrect password. Please try again.");
+          } else {
+            sessionInfo = value.otpSessionInfo;
+            $("#loader_container").removeClass("show");
+            $("#loader_container").addClass("hidden");
+            $("#user_container").addClass("hidden");
+            $("#otp_container").removeClass("hidden");
+            $("#otp_container").addClass("show");
+            console.log("Inside Pass else");
+          }
+        },
+      });
+    }
+  });
+
+  $("#pass1").focus(function () {
+    $("#pass-result").html("");
+    $("pass1").css("border", "2px solid #1f52f9");
+  });
+
+  const otpResult = message => {
+    $("#otp_error").html(message);
+  };
+
+  const loginCall = (email, authCredential) => {
+    $.ajax({
+      type: "POST",
+      data: { email, authCredential },
+      url: "loginservlet",
+      async: false,
+      success: function (value) {
+        console.log("Inside login call success", value);
+        if (value.status === true) {
+          console.log("inside logincall if");
+          window.location.href =
+            "http://localhost:8080/Authentication/profile";
+        } else if (value.status === false) {
+          otpResult(
+            "Problem in our end We will rectify it soon. Kindly try after sometime"
+          );
+        }
+      },
+    });
+  };
+
+  $("#verify_otp").click(() => {
+    const otp = $("#otp-input").val();
+    if ($.trim(otp) == null || $.trim(otp) === "") otpResult("Enter your OTP");
+    else if ($.trim(otp).length != 6) otpResult("Invalid OTP");
+    else if (otp.match(/^[0-9]+$/) == null) otpResult("Invalid OTP");
+    else {
+      $.ajax({
+        type: "POST",
+        data: { useremail: email, otp: otp, sessionInfo: sessionInfo },
+        url: "otpvalidation",
+        async: false,
+        success: function (value) {
+          console.log("In verify otp", value);
+          if (value.serverError === true)
+            otpResult(
+              "Problem in our end We will rectify it soon. Kindly try after sometime"
+            );
+          else if (value.validOTP === true)
+            loginCall(value.email, value.authCredential);
+          else otpResult("Invalid OTP");
+        },
+      });
+    }
+  });
+
+  $("#otp-input").focus(() => $("#otp_error").html(""));
+
+  $("#cancel_otp").click(() =>
+    window.location.replace("http://localhost:8080/Authentication/login")
+  );
+
+  $("resend_otp").click(() =>
+    alert("This option is not enabled. Refresh this window")
+  );
+});
+
+document.getElementById("eye").onclick = show;
+
+function show() {
+  // confirm = document.getElementById('pass2');
+  image = document.getElementById("eye");
+  var password = document.getElementById("pass1");
+  if (password.type === "password") {
+    password.type = "text";
+    // confirm.type = "text";
+    image.setAttribute("src", "eyeshow.png");
+  } else if (password.type === "text") {
+    password.type = "password";
+    // confirm.type = "password";
+    image.setAttribute("src", "eyehide.png");
+  }
+}
+
+// window.location.replace(
+//   "http://localhost:8080/Authentication/profile"
+// );
+// $("#form").submit();
---
+++ WebContent/login.jsp
@@ -0,0 +1,158 @@
+<%@page import="com.zc.accessvariables.AccessVariables"%> <%@ page
+language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="ISO-8859-1" />
+    <title>Login</title>
+
+    <link rel="stylesheet" href="./login.css" />
+    <script type="text/javascript" src="jquery/jquery-3.6.0.js"></script>
+    <script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="jquery/jquery-migrate-1.4.1.min.js"></script>
+    <script src="jquery/jquery-ui-1.10.3.custom.min.js"></script>
+  </head>
+
+  <body>
+    <% try{ Cookie[] cookies = request.getCookies(); for(Cookie cookie :cookies)
+    { if((cookie.getName()).compareTo("_Session_ID") == 0){
+    response.sendRedirect("profile"); } } } catch(Exception e){
+    response.sendRedirect("login"); } %>
+
+    <div id="user_container" class="form">
+      <form class="" action="loginservlet" method="post" id="form">
+        <div class="head">
+          <img src="logo.png" alt="logo" class="logo" />
+          <h3>Sign in</h3>
+          <h3>to access accounts</h3>
+        </div>
+        <div id="username">
+          <div class="name">
+            <input
+              class="animat"
+              type="email"
+              name="email"
+              id="userName"
+              autofocus
+              required
+            />
+            <label>Email</label>
+            <div><span id="result1"></span></div>
+          </div>
+          <div class="fub">
+            <a href="">Forget Username?</a>
+            <button type="button" id="next" class="next">Next</button>
+          </div>
+          <div id="foot-signup" style="margin-left: 11px; margin-top: 20px">
+            Don't have an account? <a href="register">Sign up now</a>
+          </div>
+        </div>
+
+        <div id="password" class="hidden">
+          <div id="pass_select">
+            <div class="pass" id="my-input">
+              <input
+                type="password"
+                name="Password"
+                class="password"
+                id="pass1"
+                autofocus
+                required
+              />
+              <label>Password</label>
+              <div><span id="pass-result"></span></div>
+            </div>
+            <div class="iconeye">
+              <img src="eyehide.png" id="eye" />
+            </div>
+            <div class="fub">
+              <a href="">Forget Password?</a>
+              <button id="btnsubmit" class="submit" type="button">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <!-- Otp container code -->
+
+    <div id="otp_container" class="otp_container hidden">
+      <div>
+        <h2>OTP Verification</h2>
+      </div>
+
+      <div class="otp_h3">
+        <h3>
+          We've sent a verification code to your email - Enter the code to
+          signin
+        </h3>
+      </div>
+      <input
+        type="text"
+        name="otp"
+        id="otp-input"
+        placeholder="Enter 6-Digit Code"
+        autofocus
+        required
+      />
+      <div id="otp_error_space" class="otp_error">
+        <span id="otp_error"></span>
+      </div>
+      <button type="button" id="resend_otp" class="otp_buttons">
+        Resend OTP
+      </button>
+      <button type="submit" id="cancel_otp" class="otp_buttons">Cancel</button>
+      <button type="button" id="verify_otp" class="otp_buttons">
+        Verify OTP
+      </button>
+    </div>
+
+    <div id="loader_container" class="hidden">
+      <div class=".loader"></div>
+    </div>
+
+    <script src="./login.js"></script>
+  </body>
+</html>
+
+<!-- <div class="login_container">
+		
+		<div class="logo">
+			<img class="logo" src="./logo.png" alt="">
+		</div>
+		<div>
+			<h3 id="h3">Sign in to Continue to you account</h3>
+		</div>
+
+		<div>
+			<form method="post" id="form">
+				<div>
+					<input type="email" name="email" id="email">
+					<div>
+						<span id="email-error">Not found account</span>
+					</div>
+				</div>
+				<div>
+					<a href="" id="create-account">Create Account</a>
+					<button type="button" id="next">Next</button>
+				</div>
+				<div>
+					<input type="password" name="password" id="password">
+					<div>
+						<span id="pass-error">Wrong password</span>
+					</div>
+				</div>
+				<div>
+					<a href="">Forget Password</a>
+					<button type="button" id="submit">Submit</button>
+				</div>
+			</form>
+		</div>
+    /* String email = AccessVariables.getEmail();
+		if(email != null){
+			response.sendRedirect("profile.jsp");
+		} */
+	</div> -->

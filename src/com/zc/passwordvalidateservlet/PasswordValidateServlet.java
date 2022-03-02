98da37de46a0d56163999ef344068f1fd61a2d7d
Raghupriyanth <raghupriyanth@gmail.com>
2022-03-02 16:17:47 +0530

    change org.json.simple.JSONObject to org.json.JSONObject

--- src/com/zc/passwordvalidateservlet/PasswordValidateServlet.java
+++ src/com/zc/passwordvalidateservlet/PasswordValidateServlet.java
@@ -14,11 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONObject;
+import org.json.JSONObject;
 
-/**
- * Servlet implementation class PasswordValidateServlet
- */
 @WebServlet("/passwordvalidate")
 public class PasswordValidateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -27,7 +24,6 @@ public class PasswordValidateServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
 
-	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("application/json");
 		

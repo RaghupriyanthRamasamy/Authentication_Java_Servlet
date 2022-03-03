c5b6a4f02c42eef0656a6c563e41a9783bec838f
Raghupriyanth <raghupriyanth@gmail.com>
2022-03-03 19:29:54 +0530

    code refactored to validate user is enrolled in mfa or not and generate id token based on same

--- src/com/zc/apihandlers/VerifyPasswordAPI/VerifyPasswordAPIServlet.java
+++ src/com/zc/apihandlers/VerifyPasswordAPI/VerifyPasswordAPIServlet.java
@@ -2,17 +2,16 @@ package com.zc.apihandlers.VerifyPasswordAPI;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.simple.JSONObject;
+import org.json.JSONObject;
 
 import com.zc.loginservlet.UserDetailClass;
-
+import com.zc.profile.ProfileDetailsClass;
 import com.zc.JWT.JsonWebToken;;
 
 @WebServlet("/api/v1/accounts/verifypassword")
@@ -33,22 +32,40 @@ public class VerifyPasswordAPIServlet extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
-		if(useremail.length() == 0 || useremail.trim() == "" || useremail == null || "\"\"".equals(useremail)) {
-			HashMap <String, String> usernameErrorObj = new HashMap <String, String>();
-			usernameErrorObj.put("error", "Invalid useremail");
-			JSONObject usernameErrorJObj = new JSONObject(usernameErrorObj);
-			out.println(usernameErrorJObj);
+		if(useremail.length() == 0 || useremail.trim().isEmpty() || "\"\"".equals(useremail)) {
+			out.println(new JSONObject().put("error", "Invalid useremail"));
 			return;
 		}
+		
+		if(password.length() == 0 || password.trim().isEmpty() || "\"\"".equals(password)) {
+			out.println(new JSONObject().put("error", "Invalid password"));
+			return;
+		}
+		
 		UserDetailClass udc = new UserDetailClass();
+		
+		if (!(udc.EmailValidate(useremail))) {
+			out.println(new JSONObject().put("error", "Account not exist: Try to Register"));
+			return;
+		}
+		
 		if(udc.PasswordValidate(useremail, password)) {
+			ProfileDetailsClass pdc = new ProfileDetailsClass();
+			JSONObject mfaEnrollmentStatus = pdc.UserMfaEnrollmentStatus(useremail);
+			if (mfaEnrollmentStatus.has("error")) {
+				out.println(mfaEnrollmentStatus);
+				return;
+			}
 			JsonWebToken jwt = new JsonWebToken();
-			out.println(jwt.JWTPartialIDToken(useremail));
+			if ((mfaEnrollmentStatus.get("mfaEnrollmentStatus")).equals(0)) {
+				out.println(jwt.IDToken(useremail, (int)mfaEnrollmentStatus.get("mfaEnrollmentStatus")));
+				return;
+			}
+			out.println(new JSONObject().put("error", "Not working"));
+
+//			out.println(jwt.JWTPartialIDToken(useremail));
 		}else {
-			HashMap <String, String> userErrorObj = new HashMap <String, String>();
-			userErrorObj.put("error", "Invalid email or password");
-			JSONObject usernameErrorJObj = new JSONObject(userErrorObj);
-			out.println(usernameErrorJObj);
+			out.println(new JSONObject().put("error", "Invalid password"));
 		}
 	}
 }

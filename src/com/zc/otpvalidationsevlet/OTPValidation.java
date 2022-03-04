6156b42ca861b7f9973defc6be0e57c5e1677860
Raghupriyanth <raghupriyanth@gmail.com>
2022-03-04 18:41:29 +0530

    Change to org.json import

--- src/com/zc/otpvalidationsevlet/OTPValidation.java
+++ src/com/zc/otpvalidationsevlet/OTPValidation.java
@@ -3,30 +3,22 @@ package com.zc.otpvalidationsevlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import com.zc.loginservlet.UserDetailClass;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONObject;
+import org.json.JSONObject;
+import com.zc.loginservlet.UserDetailClass;
 
-/**
- * Servlet implementation class OTPValidation
- */
 @WebServlet(name = "otpvalidation", urlPatterns = { "/otpvalidation" })
 public class OTPValidation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	public OTPValidation() {
-	}
+	public OTPValidation() {}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+			throws ServletException, IOException {}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -41,7 +33,5 @@ public class OTPValidation extends HttpServlet {
 		result = udc.userOTPValidation(userEmail, otp, sessionInfo);
 
 		out.println(result);
-
 	}
-
 }
